bf9720aac8bb5903cdb1ea508c462af4f71881ad
Talia <talia9tailz@pm.me>
2022-03-31 13:50:42 -0400

    Updated formatting

--- printcsv.py
+++ printcsv.py
@@ -1,23 +1,23 @@
 import csv
 
 # open spreadsheet
-with open('data.csv', newline='') as data:
+with open("data.csv", newline="") as data:
     reader = csv.reader(data)
     # iterate over rows
     for row in reader:
         # format row
-        printer = ''
+        printer = ""
         for cell in row:
-            while len(cell)<8:
-                cell = ' ' + cell
-            printer = printer + cell + '\t'
-        x=float(cell)/5
-        a='|'
-        b=''
-        c=0
-        while c<x:
-            b=b+a
-            c+=1
+            while len(cell) < 8:
+                cell = " " + cell
+            printer = printer + cell + "\t"
+        x = float(cell) / 5
+        a = "|"
+        b = ""
+        c = 0
+        while c < x:
+            b = b + a
+            c += 1
         printer = printer + b
         # print row
         print(printer)
